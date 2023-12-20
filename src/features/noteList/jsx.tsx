66c3ec43f7ad4afdccea208ec2086205a459c2eb
Mike Skoe <rapidmajk@yandex.ru>
2023-12-21 00:35:28 +0400

    Extract NoteJSX

--- src/features/noteList/jsx.tsx
+++ src/features/noteList/jsx.tsx
@@ -1,23 +1,36 @@
 import { useStoreMap } from "effector-solid";
-import { For, onMount, useContext } from "solid-js";
+import { Accessor, For, onMount, useContext } from "solid-js";
 
 import { Input, Loader, Note, Store } from "..";
+
+function NoteJSX({ note, selectedNote }: { note: Note.T, selectedNote: Accessor<Note.T["id"]>}) {
+   const actions = useContext(Store.ActionContext);
+
+   return <h2 onClick={() => actions.selectNote(note.id)}>
+      {selectedNote() === note.id ? ">" : " "} {note.title}
+   </h2>
+}
 
 export function JSX() {
    const store = useContext(Store.StoreContext);
    const actions = useContext(Store.ActionContext);
-
-   const notes = useStoreMap<Store.Root, Loader.T<Store.Notes>>(store, store => store.notes);
+   const notes = useStoreMap<Store.Root, Loader.T<Store.Notes>>(store, store =>
+      store.notes,
+   );
+   const selectedNote = useStoreMap<Store.Root, Note.T["id"]>(store, store =>
+      Loader.getWithDefault(
+         Loader.map(store.notes, ({ selected }) => selected),
+         Note.UNSORTED.id,
+      ),
+   );
 
    onMount(actions.init);
 
    return <Loader.JSX loadable={notes}>
       {notes => <>
-         <For each={notes().items}>{note =>
-            <h2 onClick={() => actions.selectNote(note.id)}>
-               {notes().selected === note.id ? ">" : " "} {note.title}
-            </h2>
-         }</For>
+         <For each={notes().items}>
+            {note => <NoteJSX note={note} selectedNote={selectedNote}/>}
+         </For>
 
          <Input.JSX onSubmit={input => actions.addNote(Note.make(input))} />
       </>}
